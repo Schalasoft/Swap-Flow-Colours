ca6775ce6267a84459c5a077fbc5b3c864598f32
Cian <47340380+Schalasoft@users.noreply.github.com>
2020-02-24 20:51:33 +0000

    Initial commit

---
+++ SwapFlowColour/SwapFlowColour.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Harmony;
+
+namespace SwapFlowColour
+{
+    [HarmonyPatch(typeof(BuildingCellVisualizerResources), "Initialize")]
+    public class BuildingCellVisualizerResources_Patch
+    {
+        public static void Postfix(ref BuildingCellVisualizerResources __instance)
+        {
+            BuildingCellVisualizerResources.ConnectedDisconnectedColours input = __instance.liquidIOColours.input;
+            __instance.liquidIOColours.input = __instance.liquidIOColours.output;
+            __instance.liquidIOColours.output = input;
+
+            BuildingCellVisualizerResources.ConnectedDisconnectedColours input2 = __instance.gasIOColours.input;
+            __instance.gasIOColours.input = __instance.gasIOColours.output;
+            __instance.gasIOColours.output = input2;
+        }
+    }
+}
